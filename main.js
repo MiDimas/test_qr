823a5c0f9607405c85a03833f61c3379d1ac2079
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-01-25 11:52:56 +0800

    Первая тестовая сборка

---
+++ main.js
@@ -0,0 +1,11 @@
+import QrScanner from "qr-scanner";
+
+// const video = document.getElementById('video');
+// const logElem = document.getElementById('log');
+// const qrScanner = QrScanner(
+//     video,
+//     result => {
+//         logElem.innerHTML = result
+//     },
+// );
+// qrScanner.start();
