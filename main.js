e1cc71efaba900bd301c0b5a2890d51a2c7787bb
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-01-25 12:37:53 +0800

    Первая рабочая версия
    корректно переподключен модуль

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-import QrScanner from "qr-scanner";
-
-// const video = document.getElementById('video');
-// const logElem = document.getElementById('log');
-// const qrScanner = QrScanner(
-//     video,
-//     result => {
-//         logElem.innerHTML = result
-//     },
-// );
-// qrScanner.start();
+// import('./node_modules/qr-scanner/qr-scanner.min.js').then((module) => {
+//     const QrScanner = module.default;
+//     console.log(QrScanner)
+//     // do something with QrScanner
+// });
+import QrScanner from "./node_modules/qr-scanner/qr-scanner.min.js";
+// console.log(QrScanner)
+const video = document.getElementById('video');
+const logElem = document.getElementById('log');
+const qrScanner = new QrScanner(
+    video,
+    result => {
+        logElem.innerHTML = result
+    },
+);
+// console.log(qrScanner)
+qrScanner.start();
